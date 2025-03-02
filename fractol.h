dbab437a4de1313872e600f90aede9949eebd0a7
rmakende <rmakendenguerodriguez@gmail.com>
2025-03-02 22:34:49 +0100

    update

--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: rmakende <rmakende@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 12:44:03 by rmakende          #+#    #+#             */
-/*   Updated: 2025/03/02 21:33:12 by rmakende         ###   ########.fr       */
+/*   Updated: 2025/03/02 22:34:18 by rmakende         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,31 +24,31 @@
 # include <string.h>
 # include <unistd.h>
 
-# define WIDTH 800
+# define WIDTH 1000
 # define HEIGHT 800
 # define DELTA 0.05
 
-# define BLACK 0x000000              // RGB(0, 0, 0)
-# define WHITE 0xFFFFFF              // RGB(255, 255, 255)
-# define RED 0xE50000                // RGB(229, 0, 0)
-# define GREEN 0x00CC00              // RGB(0, 204, 0)
-# define BLUE 0x0033CC               // RGB(0, 51, 204)
-# define MAGENTA_BURST 0xD100D1      // Intense magenta
-# define LIME_SHOCK 0xBFFF00         // Neon lime
-# define NEON_ORANGE 0xFF4500        // Bright neon orange
-# define PSYCHEDELIC_PURPLE 0x800080 // Deep purple
-# define AQUA_DREAM 0x00FFFF         // Vibrant cyan
-# define HOT_PINK 0xFF1493           // Bright pink
-# define ELECTRIC_BLUE 0x1E90FF      // Striking blue
-# define LAVA_RED 0xFF2400           // Intense molten red
-# define ULTRA_VIOLET 0x9400D3       // Vivid violet
-# define CYBER_YELLOW 0xFFD700       // Bright cyber yellow
-# define DEEP_TEAL 0x008080          // Dark teal
-# define RADIANT_TURQUOISE 0x40E0D0  // Bright turquoise
-# define BURNING_GOLD 0xFFA500       // Intense golden orange
-# define TOXIC_GREEN 0x39FF14        // Fluorescent green
-# define MIDNIGHT_BLUE 0x191970      // Deep blue
-# define SHOCKING_PURPLE 0x9932CC    // Eye-catching purple
+# define BLACK 0x000000
+# define WHITE 0xFFFFFF
+# define RED 0xE50000
+# define GREEN 0x00CC00
+# define BLUE 0x0033CC
+# define MAGENTA_BURST 0xD100D1
+# define LIME_SHOCK 0xBFFF00
+# define NEON_ORANGE 0xFF4500
+# define PSYCHEDELIC_PURPLE 0x800080
+# define AQUA_DREAM 0x00FFFF
+# define HOT_PINK 0xFF1493
+# define ELECTRIC_BLUE 0x1E90FF
+# define LAVA_RED 0xFF2400
+# define ULTRA_VIOLET 0x9400D3
+# define CYBER_YELLOW 0xFFD700
+# define DEEP_TEAL 0x008080
+# define RADIANT_TURQUOISE 0x40E0D0
+# define BURNING_GOLD 0xFFA500
+# define TOXIC_GREEN 0x39FF14
+# define MIDNIGHT_BLUE 0x191970
+# define SHOCKING_PURPLE 0x9932CC
 
 typedef struct s_image
 {
